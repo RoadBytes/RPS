774d9059133a0ec5337d2701868d6b0aaebac302
Jason Data <jason.data@roadbytes.me>
2016-07-30 13:14:24 -0700

    added test for Player#choose

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe Player do
-  describe "#choose" do
-    it "takes in input" do
-      human = Player.new("Bob")
-      human.move = 
-      expect(human).to eq 1
+  describe '#choose' do
+    it 'takes in input' do
+      human = Player.new('Bob')
+      allow(human).to receive(:gets).and_return("paper\n")
+
+      move = human.choose
+
+      expect(move.value).to eq 'paper'
     end
   end
 end
